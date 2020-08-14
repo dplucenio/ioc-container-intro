4273f453000777b8202a6cb2990644b1bfb7f84d
Daniel Plucenio <dplucenio@gmail.com>
2020-08-14 18:20:30 -0300

    Adding properties and using values

--- src/main/java/io/plucen/AppConfiguration.java
+++ src/main/java/io/plucen/AppConfiguration.java
@@ -1,13 +1,19 @@
 package io.plucen;
 
 import external.NumberService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackageClasses = {AppConfiguration.class, NumberService.class})
+@PropertySource("classpath:/application.properties")
+@PropertySource("classpath:/other.properties")
 public class AppConfiguration {
+  @Value("${greetingStart}")
+  String greetingStart;
 
   @Bean
   public Integer getDefaultNumber() {
@@ -15,7 +21,7 @@ public class AppConfiguration {
   }
 
   @Bean
-  public String getDefaultGreeting() {
-    return "Hello I come from the IoC container!";
+  public String getDefaultGreeting(@Value("${greetingEnd}") String greetingEnd) {
+    return greetingStart + " " + greetingEnd;
   }
 }
